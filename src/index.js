00887fb0a8bf6556ffcf4c8e6d63ce819f431da0
ankit <ankitdetroja@gmail.com>
2021-02-07 12:53:50 +0530

    New commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const config = require("./config/config");
 const port = process.env.PORT || 5002;
 
 const app = express();
+app.set("view engine","jade")
 app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
