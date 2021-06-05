c97a2ae2013baab314910ae5b7ccd1bd6fc1ceab
K.d_pandya <kpandya353@gmail.com>
2021-06-05 15:55:37 +0530

    fix:single retrun

--- src/module/customer/customer.controller.js
+++ src/module/customer/customer.controller.js
@@ -3,40 +3,30 @@ const logger = require('../../logger/logger');
 
 const fetchCustomerDetails = function (req, res) {
     logger.info(`fetching customer data`)
-    customerService.fetchCustomerDetails(req, res).then((response) => {
-        res.send(response);
-    });
+    customerService.fetchCustomerDetails(req, res).then((response) => res.send(response));
 }
 
 const updateCustomerDetails = function (req, res) {
     logger.info(`updating customer data`)
-    customerService.updateCustomerDetails(req, res).then((response) => {
-        res.send(response);
-    });
+    customerService.updateCustomerDetails(req, res).then((response) => res.send(response));
 }
 
 const  deleteCustomerDetails = function (req,res) {
     logger.info(`deleting customer data`)
-    customerService.deleteCustomerDetails(req, res).then((response) => {
-        res.send(response);
-    });
+    customerService.deleteCustomerDetails(req, res).then((response) => res.send(response));
 }
 const createCustomerDetails = function (req,res) {
     logger.info(`creating customer data`)
-    customerService.createCustomerDetails(req, res).then((response) => {
-        res.send(response);
-    });
+    customerService.createCustomerDetails(req, res).then((response) => res.send(response));
 }
 const searchCustomerById = function (req,res) {
     logger.info(`fetching customer data`)
-    customerService.searchCustomerById(req, res).then((response) => {
-        res.send(response);
-    }); 
+    customerService.searchCustomerById(req, res).then((response) => res.send(response)); 
 }
 
 module.exports = {
-    fetchCustomerDetails : fetchCustomerDetails,
-    updateCustomerDetails :updateCustomerDetails,
+    fetchCustomerDetails,
+    updateCustomerDetails,
     deleteCustomerDetails,
     searchCustomerById,
     createCustomerDetails
--- src/module/user/user.controller.js
+++ src/module/user/user.controller.js
@@ -4,23 +4,17 @@ require('dotenv').config();
 
 const sendMail = function (req, res) {
     logger.info(`Sending email to ${JSON.stringify(req.body)}`)
-    userService.sendMail(req, res).then((response) => {
-        res.send(response);
-    });
+    userService.sendMail(req, res).then((response) => res.send(response));
 }
 
 const sendLink = function (req, res) {
     logger.info(`Sending downloadind link via Mail`)
-    userService.sendLink(req,res).then((response) => {
-        res.send(response)
-    });
+    userService.sendLink(req,res).then((response) => res.send(response));
 }
 
 const contactUs = (req, res) => {
     logger.info(`Contact to customer with data ${JSON.stringify(req.body)}`);
-    userService.contactUs(req, res).then((response) => {
-        res.send(response);
-    });
+    userService.contactUs(req, res).then((response) => res.send(response));
 }
 
 module.exports = {
