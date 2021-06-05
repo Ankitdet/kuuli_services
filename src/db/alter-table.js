d4c60fa838344f7ce6e578ba4d491414642a8e6d
Ankit Detroja <ankitdetroja@HUBLAPINDCE-53.local>
2021-06-05 12:31:29 +0530

    fix: alter and drop query

--- src/db/alter-table.js
+++ src/db/alter-table.js
@@ -10,17 +10,34 @@ const pool = new Pool(config)
 
 const query = `
 
-ALTER TABLE customer_details ALTER COLUMN customer_id SET DEFAULT NOT NULL;
-ALTER TABLE customer_details ALTER COLUMN title   SET DEFAULT NOT NULL;
-ALTER TABLE customer_details ALTER COLUMN department   SET DEFAULT NOT NULL;
-ALTER TABLE customer_details ALTER COLUMN company_name  SET DEFAULT NOT NULL;
-ALTER TABLE customer_details ALTER COLUMN revenue   SET DEFAULT NOT NULL;
-ALTER TABLE customer_details ALTER COLUMN phone_number  SET DEFAULT NOT NULL;
-ALTER TABLE customer_details ALTER COLUMN mobile_number  SET DEFAULT NOT NULL;
-
-
+ALTER TABLE customer_details ALTER COLUMN customer_id SET DEFAULT NULL;
+ALTER TABLE customer_details ALTER COLUMN title   SET DEFAULT NULL;
+ALTER TABLE customer_details ALTER COLUMN department   SET DEFAULT NULL;
+ALTER TABLE customer_details ALTER COLUMN company_name  SET DEFAULT NULL;
+ALTER TABLE customer_details ALTER COLUMN revenue   SET DEFAULT NULL;
+ALTER TABLE customer_details ALTER COLUMN phone_number  SET DEFAULT NULL;
+ALTER TABLE customer_details ALTER COLUMN mobile_number  SET DEFAULT NULL;
 
 ALTER TABLE onload_ca ALTER COLUMN service SET DEFAULT NOT NULL;
+
+ALTER TABLE quotation DROP COLUMN IF EXISTS container_size;
+ALTER TABLE quotation DROP COLUMN IF EXISTS container_quantity;
+ALTER TABLE quotation DROP COLUMN IF EXISTS container_weight;
+
+ALTER TABLE quotation ADD IF NOT EXISTS tw_ft_container VARCHAR(100) DEFAULT NULL;
+ALTER TABLE quotation ADD  IF NOT EXISTS ft_ft_container VARCHAR(100) DEFAULT NULL;
+ALTER TABLE quotation ADD  IF NOT EXISTS  ft_ft_high_cube VARCHAR(100) DEFAULT NULL;
+ALTER TABLE quotation ADD  IF NOT EXISTS  customer_id VARCHAR(100) DEFAULT NULL;
+
+DO $$
+BEGIN
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='quotation' and column_name='type')
+  THEN
+      ALTER TABLE quotation RENAME COLUMN "type" TO "terms";
+  END IF;
+END $$;
 `
 
 pool.connect((err, client, done) => {
--- src/services/dashboard/dashboard.service.js
+++ src/services/dashboard/dashboard.service.js
@@ -1,65 +1,60 @@
 
-require('dotenv').config();
-var csv = require('csv-parser')
-var fs = require('fs');
-const path = require('path');
-const moment = require('moment');
-const executeQuery = require('../../db/connect');
-var readLine = require('readline');
-const { NOT_FOUND, INTERNAL_SERVER_ERROR, OK } = require('../../utils/apiStatus');
+require('dotenv').config()
+const moment = require('moment')
+const executeQuery = require('../../db/connect')
+const { NOT_FOUND, INTERNAL_SERVER_ERROR, OK } = require('../../utils/apiStatus')
 
 const onLoadQuotations = async (req, res) => {
-    let query = `SELECT * FROM onload_quotation`;
+  const query = 'SELECT * FROM onload_quotation'
 
-    try {
-        return executeQuery(query).then((data) => {
+  try {
+    return executeQuery(query).then((data) => {
+      const containerType = []
+      const incoterms = []
+      const terms = []
+      const ports = []
 
-            let containerType = []
-            let incoterms = []
-            let terms = []
-            let ports = []
+      data.rows.forEach((ca) => {
+        if (ca.container_type !== '') {
+          containerType.push({
+            label: ca.container_type,
+            value: ca.container_type
+          })
+        }
+        if (ca.ports !== '') {
+          ports.push({
+            label: ca.ports,
+            value: ca.ports
+          })
+        }
 
-            data.rows.forEach((ca) => {
-                if (ca.container_type != '') {
-                    containerType.push({
-                        label: ca.container_type,
-                        value: ca.container_type
-                    });
-                }
-                if (ca.ports != "") {
-                    ports.push({
-                        label: ca.ports,
-                        value: ca.ports
-                    })
-                }
+        if (ca.incoterms !== '') {
+          incoterms.push({
+            label: ca.incoterms,
+            value: ca.incoterms
+          })
+        }
 
-                if (ca.incoterms != '') {
-                    incoterms.push({
-                        label: ca.incoterms,
-                        value: ca.incoterms
-                    })
-                }
+        if (ca.terms !== '') {
+          terms.push({
+            label: ca.terms,
+            value: ca.terms
+          })
+        }
+      })
 
-                if (ca.terms != '') {
-                    terms.push({
-                        label: ca.terms,
-                        value: ca.terms
-                    })
-                }
-            })
-
-            let json = {
-                "portOfLoading": ports,
-                "portOfDischarge": ports,
-                "containerType": containerType,
-                "incoterms": incoterms,
-                "terms": terms
-            }
-            res.status(OK).send({ data: json, message: 'fetched.' });
-        });
-    } catch (err) {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: err });
-    }
+      const json = {
+        portOfLoading: ports,
+        portOfDischarge: ports,
+        containerType: containerType,
+        incoterms: incoterms,
+        terms: terms
+      }
+      res.status(OK).send({ data: json, message: 'fetched.' })
+    })
+  } catch (err) {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: err })
+  }
 }
 
 const getDataFromExcelSheet = async (req, res) => {
@@ -67,102 +62,86 @@ const getDataFromExcelSheet = async (req, res) => {
 }
 
 const createQuotation = async (req, res) => {
-
-    const { origin, destination, containerName, containerType, containerSize, containerQuantity, containerWeight, cargoReadyDate, incoterms, type } = req.body;
-    let query = `INSERT INTO quotation("origin", "destination", "container_name", "container_type", "container_size", "container_quantity", "container_weight", "cargo_ready_date", "incoterms", "type", "created_on") 
-            VALUES ('${origin}', '${destination}', '${containerName}', '${containerType}', '${containerSize}', '${containerQuantity}', '${containerWeight}', ''${moment(cargoReadyDate).format('YYYY-MM-DD HH:mm:ss')}', '${incoterms}', '${type}', '${moment(new Date()).format('YYYY-MM-DD HH:mm:ss')}')`;
-    try {
-        return executeQuery(query).then((data) => {
-            res.status(OK).send({ message: 'Quotation successfully inserted.' });
-        });
-    } catch (err) {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: err });
-    }
+  const { origin, destination, containerName, containerType, twentyFeetContainer, fourtyFeetContainer, fourtyFeetHighCube, cargoReadyDate, incoterms, terms, customerId} = req.body
+  const query = `INSERT INTO quotation("origin", "destination", "container_name", "container_type", 
+            "tw_ft_container", "ft_ft_container", "ft_ft_high_cube", "cargo_ready_date",
+            "incoterms", "terms", "created_on", "customer_id") 
+            VALUES ('${origin}', '${destination}', '${containerName}', '${containerType}', 
+            '${twentyFeetContainer}', '${fourtyFeetContainer}', '${fourtyFeetHighCube}', 
+            '${moment(cargoReadyDate).format('YYYY-MM-DD HH:mm:ss')}', '${incoterms}', 
+            '${terms}', '${moment(new Date()).format('YYYY-MM-DD HH:mm:ss')}', '${customerId}')`
+  try {
+    return executeQuery(query).then((data) => {
+        console.log('query:',query);
+      res.status(OK).send({ message: 'Quotation successfully inserted.' })
+    })
+  } catch (err) {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: err })
+  }
 }
 
 const fetchQuotation = async (req, res) => {
-    let query = `SELECT * FROM quotation;`;
-    try {
-        return executeQuery(query).then((data) => {
-            res.status(OK).send({ data: data.rows });
-        });
-    } catch (err) {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: err });
-    }
+  const query = 'SELECT * FROM quotation;'
+  try {
+    return executeQuery(query).then((data) => {
+      res.status(OK).send({ data: data.rows })
+    })
+  } catch (err) {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: err })
+  }
 }
 
 const updateQuotation = async (req, res) => {
+  const { qId, origin, destination, containerName, containerType, twentyFeetContainer, fourtyFeetContainer, fourtyFeetHighCube, cargoReadyDate, incoterms, terms, customerId } = req.body
+  if (!qId) res.status(NOT_FOUND).send({ message: 'Id not found during update operation.' })
+  const query = `UPDATE "quotation" 
+            SET 
+            "origin"='${origin}', 
+            "destination"='${destination}', 
+            "container_name"='${containerName}', 
+            "container_type"='${containerType}', 
+            "tw_ft_container"='${twentyFeetContainer}', 
+            "ft_ft_container"='${fourtyFeetContainer}', 
+            "ft_ft_high_cube"='${fourtyFeetHighCube}',
+            "customer_id"='${customerId}',
+            "cargo_ready_date"='${moment(cargoReadyDate).format('YYYY-MM-DD HH:mm:ss')}', 
+            "incoterms"='${incoterms}', 
+            "terms"='${terms}', 
+            "updated_on"='${moment(new Date()).format('YYYY-MM-DD HH:mm:ss')}' 
+            WHERE "q_id"='${qId}'`
 
-    const { qId, origin, destination, containerName, containerType, containerSize, containerQuantity, containerWeight, cargoReadyDate, incoterms, type } = req.body;
-    if (!qId) res.status(NOT_FOUND).send({ message: "Id not found during update operation." });
-    let query = `UPDATE "quotation" SET "origin"='${origin}', "destination"='${destination}', 
-    "container_name"='${containerName}', "container_type"='${containerType}', 
-    "container_size"='${containerSize}', "container_quantity"='${containerQuantity}', "container_weight"='${containerWeight}', 
-    "cargo_ready_date"='${moment(cargoReadyDate).format('YYYY-MM-DD HH:mm:ss')}', "incoterms"='${incoterms}', "type"='${type}', "updated_on"='${moment(new Date()).format('YYYY-MM-DD HH:mm:ss')}' WHERE "q_id"='${qId}'`;
-
-    try {
-        return executeQuery(query).then((data) => {
-            if (data.rowCount == 0) {
-                res.status(OK).send({ rowCount: data.rowCount, message: 'Not data updated.' });
-            } else {
-                res.status(OK).send({ rowCount: data.rowCount, message: 'Quotation successfully updated.' });
-            }
-        });
-    } catch (err) {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: err });
-    }
+  try {
+    return executeQuery(query).then((data) => {
+      if (data.rowCount === 0) {
+        res.status(OK).send({ rowCount: data.rowCount, message: 'Not data updated.' })
+      } else {
+        res.status(OK).send({ rowCount: data.rowCount, message: 'Quotation successfully updated.' })
+      }
+    })
+  } catch (err) {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: err })
+  }
 }
 
 const fetchQuotationById = async (req, res) => {
-    let id = req.query.id;
-    if (!id) res.status(NOT_FOUND).send({ message: "Id not found to fetch the data" });
-    let query = `SELECT * FROM quotation where q_id=${id}`;
-    try {
-        return executeQuery(query).then((data) => {
-            res.status(OK).send({ data: data.rows, message: 'Quotation successfully fectched.' });
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(INTERNAL_SERVER_ERROR).send({ message: err });
-    }
-}
-
-var data = [];
-function fnc(line, number) {
-    data.push(line);
-}
-
-function readFromN2M(fileName, n, m, func) {
-    const lineReader = readLine.createInterface({
-        input: fs.createReadStream(fileName)
+  const id = req.query.id
+  if (!id) res.status(NOT_FOUND).send({ message: 'Id not found to fetch the data' })
+  const query = `SELECT * FROM quotation where q_id=${id}`
+  try {
+    return executeQuery(query).then((data) => {
+      res.status(OK).send({ data: data.rows, message: 'Quotation successfully fectched.' })
     })
-
-    let lineNumber = 0;
-    fs.createReadStream(fileName)
-        .pipe(csv())
-        .on(
-            'data', (row) => {
-                lineReader.on('line', (line) => {
-                    lineNumber++;
-                    if (lineNumber >= n && lineNumber < m) {
-                        func(row, lineNumber);
-                    }
-                });
-            }
-        ).on('end', () => {
-            console.log(data);
-            console.log('Data loaded');
-        })
+  } catch (err) {
+    console.log(err)
+    res.status(INTERNAL_SERVER_ERROR).send({ message: err })
+  }
 }
-
-// readFromN2M(path.resolve(__dirname) + '../../../../resources/BTC-USD.csv', 1, 1000, fnc);
-
 
 module.exports = {
-    getDataFromExcelSheet,
-    createQuotation,
-    fetchQuotationById,
-    updateQuotation,
-    fetchQuotation,
-    onLoadQuotations
+  getDataFromExcelSheet,
+  createQuotation,
+  fetchQuotationById,
+  updateQuotation,
+  fetchQuotation,
+  onLoadQuotations
 }
